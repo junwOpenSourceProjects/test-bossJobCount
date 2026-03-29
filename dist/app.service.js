9ceb6042786e173b1bb4633a6f2b9dadbfed9027
junw <wo1261931780@gmail.com>
2026-03-29 14:46:16 +0800

    chore: 依赖升级到最新版本
    - 升级Spring Boot到3.4.10/3.5.3
    - 升级MyBatis-Plus到3.5.16
    - 升级Vite到8.0.3
    - 升级TypeScript到6.0.2
    - 升级Vue到3.5.31
    - 升级Element Plus到2.13.6
    - 修复TypeScript 6.0兼容性问题
    - 修复Vite 8兼容性问题
    - 修复Tailwind CSS 4迁移问题
    - 清理废弃代码和备份文件

--- dist/app.service.js
+++ dist/app.service.js
@@ -8,10 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
-const puppeteer_1 = require("puppeteer");
+const puppeteer_1 = __importDefault(require("puppeteer"));
 const typeorm_1 = require("typeorm");
 const Job_1 = require("./entites/Job");
 let AppService = class AppService {
